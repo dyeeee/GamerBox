5cfcfe889240a29b2e9b3afe48191575ecd4471e
yzhb346 <100895681+yzhb346@users.noreply.github.com>
2022-04-26 15:58:28 +0800

    添加热门游戏的数据

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,15 @@ const router = express.Router();
 router.get('/fetchData', async(req, res)=> {
     const data = await GameData.find({});
     res.json(data);
+    console.log("123123312"+data);
+});
+
+router.get('/fetchData/:gameId', async(req, res)=> {
+    const {gameId} = req.params;
+
+    const data = await GameData.find({GameID: gameId});
+    res.json(data);
+    console.log("123123312"+data);
 });
 
 router.use('/steamApi',steamApi);
